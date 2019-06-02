772cf50e9d52e54407e504f5db488bfb1c4eb6cc
Luiz Pereira <k27287@karsten.com.br>
2019-06-02 11:10:44 -0300

    my FirstWebApp

---
+++ FirstWebApp/FirstWebApp/Data/ContextApp.cs
@@ -0,0 +1,25 @@
+﻿using FirstWebApp.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace FirstWebApp.Data
+{
+    public class ContextApp : DbContext
+    {
+        public ContextApp()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public virtual DbSet<Product> Product { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Properties<string>().Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties().Where(p => p.Name == "Id").Configure(p => p.IsKey());
+        }
+    }
+}
