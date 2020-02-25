0f2eecf6198f5df4a06b3023edd356f516d2905b
LuizRoyer <32273858+LuizRoyer@users.noreply.github.com>
2020-02-25 18:30:58 -0300

    Api de Contato

---
+++ ContatosApi/ContatosApi/Migrations/20200122000820_PopularContatosDb.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContatosApi.Migrations
+{
+    public partial class PopularContatosDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Contatos(Nome, Email,Telefone) Values('Luiz','Luiz.felipe@gmail.com','33349910')");
+            migrationBuilder.Sql("Insert into Contatos(Nome, Email,Telefone) Values('pedro','pedro.souza@gmail.com','333455910')");
+            migrationBuilder.Sql("Insert into Contatos(Nome, Email,Telefone) Values('junior','junior.alvez@gmail.com','33459910')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete From Contatos");
+
+        }
+    }
+}
---
+++ ContatosApi/ContatosApi/Models/Contato.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContatosApi.Models
+{
+    [Table("Contatos")]
+    public class Contato
+    {[Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Campo Nome é Obrigatorio")]
+        [StringLength(100)]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Campo Email é Obrigatorio")]
+        [StringLength(150)]
+        [EmailAddress(ErrorMessage ="Email Invalido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Campo Telefone é Obrigatorio")]
+        [StringLength(15)]
+        public string Telefone { get; set; }
+    }
+}
