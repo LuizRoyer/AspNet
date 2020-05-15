2cc0380cd501c07acf3bc11f9cfad8a304c3dd56
LuizRoyer <32273858+LuizRoyer@users.noreply.github.com>
2020-05-14 21:23:40 -0300

    Add files via upload
    Projeto controle de Fornecedor

---
+++ Fornecedores/Fornecedores/Repositories/Connections/Connection.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace Fornecedores.Repositories.Connections
+{
+    public class Connection
+    {
+        const string connSQL = "Data Source=HP\\SQLEXPRESS;Initial Catalog=bd;Integrated Security=True;";
+
+        public static SqlConnection ConnectionSQL()
+        {
+            return new SqlConnection(connSQL);
+        }
+    }
+}
